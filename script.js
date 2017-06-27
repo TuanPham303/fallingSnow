6440d7e399fb61d7b250815fbebe4d160937a616
TuanPham303 <phamcongtuan303@gmail.com>
2017-06-26 19:37:04 -0700

    motion is nore real

--- script.js
+++ script.js
@@ -1,46 +1,44 @@
-var canvas = document.getElementById('field')
-canvas.width = window.innerWidth
+var canvas = document.getElementById('canvas')
 canvas.height = window.innerHeight
+canvas.width = window.innerWidth
 var ctx = canvas.getContext('2d')
 
 function snow(){
-   this.x=Math.round(Math.random()*canvas.width)/*snow position on x axis*/
-   this.y=Math.round(Math.random()*canvas.height)/*snow position on y axis*/
-   this.r=Math.round(Math.random())*3+2 /*snow radius*/
-   this.wind=Math.random()*5/*snow move around*/
+   this.x = Math.round(Math.random()*canvas.width)
+   this.y = 0
+   this.yspeed = Math.random()*5+3
+   this.r = Math.round(Math.random()*5)
+   this.wind = Math.round(Math.random()*5)
 }
 
-snow.prototype.update=function(){
-   ctx.fillStyle="#fff"
+snow.prototype.update = function(){
    ctx.beginPath()
-   ctx.arc(this.x,this.y,this.r,0,Math.PI*2,false)
+   ctx.arc(this.x,this.y,this.r,0,2*Math.PI)
+   ctx.fillStyle = '#fff'
    ctx.fill()
-   ctx.closePath()
-   this.x+=Math.sin(this.wind)/*snow move in x axis*/
-   this.wind+=0.001
-   if (this.x>canvas.width||this.x<0) {
-      this.x=Math.round(Math.random()*canvas.width)
-      this.y=0
-   }/*if snow goes out of screen, make a new one*/
-   this.y+=5
-   if (this.y>canvas.height) {
-      this.y=0
-   }/*if snow touch bottom screen, make new one at the top*/
+   this.y += this.yspeed
+   this.x += Math.sin(this.wind)
+   this.wind += 0.01
+   if(this.x<0 || this.x>canvas.width){
+      this.x = Math.round(Math.random()*canvas.width)
+      this.y = 0
+   }
+   if(this.y > canvas.height){
+      this.y = 0
+   }
 }
-
-var snowNumber = 500
+var snowNum = 1000
 var snows = []
 
 function fallingSnow(){
    ctx.clearRect(0,0,canvas.width,canvas.height)
-   for (var i = 0; i < snowNumber; i++) {
+   for (var i = 0; i < snowNum; i++) {
       snows[i].update()
    }
-}
-
-window.onload = function(){
-   for(i=0;i<snowNumber;i++){
+} 
+window.onload = function main(){
+   for (var i = 0; i < snowNum; i++) {
       snows.push(new snow())
-   } 
-   setInterval(fallingSnow, 50) 
+   }
+   setInterval(fallingSnow,50)
 }
